ed9ad8873cd099b0b7c28c65f4ba68e1258d6834
keyboardcat5 <wushi6555@gmail.com>
2025-01-11 15:53:29 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 from app import app
 
-# 这行是给 Vercel 使用的
-app.debug = False
+# 这是 Vercel 需要的
+application = app
 
+# 本地运行时使用
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=8080)
+    app.run()
